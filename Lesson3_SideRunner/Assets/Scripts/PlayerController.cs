78f5b881550b0652bdecc1ab2d1a96dbb216eaa7
theKorax <zikoramadin@gmail.com>
2023-09-06 16:48:01 +0100

    Updated PlayerController.cs

--- Lesson3_SideRunner/Assets/Scripts/PlayerController.cs
+++ Lesson3_SideRunner/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
