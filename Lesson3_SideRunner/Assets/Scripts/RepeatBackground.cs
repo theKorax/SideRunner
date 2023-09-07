8372fa5a204f04f7bde6647a95ce5131c6b273c1
theKorax <zikoramadin@gmail.com>
2023-09-07 10:49:34 +0100

    Updated RepeatBackground.cs

--- Lesson3_SideRunner/Assets/Scripts/RepeatBackground.cs
+++ Lesson3_SideRunner/Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startPos;
+    private float offset = 50.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < startPos.x - offset)
+        {
+            transform.position = startPos;
+        }
     }
 }
