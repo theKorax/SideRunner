13134168b74ada2daf47e4f77d2c72971e538df8
theKorax <zikoramadin@gmail.com>
2023-09-07 14:10:39 +0100

    Updated PlayerController.cs
    Updated the script to stop the dirt particle effect from playing whenever the player had jumped or had died.

--- Lesson3_SideRunner/Assets/Scripts/PlayerController.cs
+++ Lesson3_SideRunner/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     public float jumpForce = 10.0f;
     public float gravityModifier;
     private bool isOnGround = true;
@@ -27,6 +28,7 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
 
@@ -35,6 +37,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
@@ -42,6 +45,7 @@ public class PlayerController : MonoBehaviour
             explosionParticle.Play();
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            dirtParticle.Stop();
         }
     }
 }
